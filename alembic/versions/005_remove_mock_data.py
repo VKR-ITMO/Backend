e78079172993e0f6b293eaa5aff530de12772e62
Sher <artem.sher21.11@mail.ru>
2026-05-12 03:11:56 +0300

    fix: add no-op stub migration 005 to restore Alembic chain
    - Database is at revision 005 but file was deleted
    - Create no-op stub to fix deployment error

---
+++ alembic/versions/005_remove_mock_data.py
@@ -0,0 +1,24 @@
+"""Remove mock data from database
+
+Revision ID: 005
+Revises: 004
+Create Date: 2026-05-12
+"""
+from alembic import op
+
+
+# revision identifiers
+revision = '005'
+down_revision = '004'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # No-op stub - migration was previously applied and deleted
+    pass
+
+
+def downgrade():
+    # No-op stub
+    pass
